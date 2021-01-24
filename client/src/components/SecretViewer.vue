fca24555d93e5dc96c3b09e80d0892f8f88c8ac4
lil-armstrong <oebong1@gmail.com>
2021-01-25 00:17:46 +0100

    Connected client to server

--- client/src/components/SecretViewer.vue
+++ client/src/components/SecretViewer.vue
@@ -1,38 +1,43 @@
 <template>
   <section>
-    <div>
-      Info:{{info}}
-    </div>
-    <div>
-      hash:{{hash}}
-    </div>
+    <div>Info:{{ info }}</div>
+    <div>hash:{{ hash }}</div>
   </section>
 </template>
 
 <script>
-  export default {
-    name: 'secret-viewer',
-    props: {
-      hash: {
-        required: true,
-        type: String
-      }
+export default {
+  name: "secret-viewer",
+  props: {
+    hash: {
+      required: true,
+      type: String,
     },
-    data() {
-      return {
-        info: null
-      }
-    },
-    mounted () {
-      //todo: get the id
-      if (this.hash)
-        this.axios({
-        method: 'get',
+  },
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    //todo: get the id
+    if (this.hash)
+      this.axios({
+        method: "get",
         url: `/${this.hash}`,
-        responseType: 'stream'
-      }).then(response=> {
-        this.info = response.data
       })
-    }
-  }
-  </script>
+        .then((response) => {
+          this.info = response.data;
+        })
+        .catch((error) => {
+          if (error.response) {
+            console.error(error.response.statusText);
+          } else if (error.request) {
+            console.error({ request: error.response });
+          } else {
+            console.error({ error });
+          }
+        });
+  },
+};
+</script>
