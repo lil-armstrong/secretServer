e2945844531b9845b96f1ab541c758d3960bdf1d
lil-armstrong <oebong1@gmail.com>
2021-01-28 13:05:58 +0100

    Refactoring server/index.js

--- server/index.js
+++ server/index.js
@@ -1,44 +1,31 @@
-const dbConnection = require('./config/mongodb.config.js');
+const { mongoose, uri, options } = require('./config/mongodb.config.js');
 const express = require('express');
-const url = require('url');
-const session = require('express-session');
 const cors = require('cors');
-const {
-  v4: uuidv4
-} = require('uuid');
 const router = require('./router');
-const models = require('./models')
+
 const port = 8000;
 const app = express();
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json());
+app.use(cors({
+  origin: '*'
+}))
 
+// // Routing
+router(app);
 
-const serve = function () {
-  dbConnection.once('open', () => {
-    app.use(express.urlencoded({
-      extended: true
-    }))
-    app.use(express.json());
-    app.use(cors({
-      origin: '*'
-    }))
-    // app.use(session({
-    //   cookie: {
-    //     maxAge: 3600000, sameSite: true
-    //   },
-    //   secret: uuidv4(),
-    //   saveUninitialized: true,
-    //   resave: false
-    // }))
-    // // Routing
-    router(app);
-
-    // Start server
-    app.listen({
-      port
-    }, () => {
+(async () => {
+  try {
+    const connection = await mongoose.connect(uri, options);
+    app.listen(port, () => {
       console.log(`Server listening on port ${port}`)
     })
-  });
-}
-
-serve()
+  } catch (err) {
+    if (process.env.NODE_DEBUG) {
+      console.log(err)
+    } else
+      console.log('Mongoose connection error');
+  }
+})()
